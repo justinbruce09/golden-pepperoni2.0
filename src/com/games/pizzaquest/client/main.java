36b07055ec5a03078903ca84ddff8b4382cb167f
mlmirk <mike.l.mirkovic@gmail.com>
2022-06-08 13:28:15 -0700

    build out app class with basic welcome banner, AB#53

--- src/com/games/pizzaquest/client/main.java
+++ src/com/games/pizzaquest/client/main.java
@@ -1,7 +1,11 @@
 package com.games.pizzaquest.client;
 
+import com.games.pizzaquest.client.app.PizzaQuestApp;
+
 class main {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        PizzaQuestApp app = new PizzaQuestApp();
+        app.execute();
     }
 }
