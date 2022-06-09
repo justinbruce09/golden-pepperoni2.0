e046a55dd146ac9005b222cb187c6fa219b8dfe3
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-06-09 13:37:17 -0700

    added logic for look, take, give, and inventory, AB#58

--- src/com/games/pizzaquest/objects/Location.java
+++ src/com/games/pizzaquest/objects/Location.java
@@ -1,13 +1,22 @@
 package com.games.pizzaquest.objects;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Location {
     private String name;
     private List<Location> adjacentLocations;
 
-    public Location (String name){
+    public Location (String name, String... locations){
         this.name = name;
+        setAdjacentLocations(locations);
+    }
+
+    public void setAdjacentLocations(String... locations) {
+        adjacentLocations = new ArrayList<>();
+        for (String location : locations) {
+            this.adjacentLocations.add(new Location(location));
+        }
     }
 
     public String getName() {
