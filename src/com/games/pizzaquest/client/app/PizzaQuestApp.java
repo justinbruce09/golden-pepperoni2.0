4ff72e2e88efc37311a3bfa13db517f51c45eb28
mlmirk <mike.l.mirkovic@gmail.com>
2022-06-09 08:24:17 -0700

    stub up verb noun methods, AB#57

--- src/com/games/pizzaquest/client/app/PizzaQuestApp.java
+++ src/com/games/pizzaquest/client/app/PizzaQuestApp.java
@@ -4,18 +4,21 @@ import com.games.pizzaquest.textparser.TextParser;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PizzaQuestApp {
 
         //scanner for the game
-        private Scanner scanner = new Scanner(System.in);
+        static Scanner scanner = new Scanner(System.in);
         //text parser for users to use
-        TextParser tp = new TextParser();
         //path for some ascii art
         private static final String bannerFilePath = "data/WelcomeSplash.txt";
         //track turn may be moved to player
         private int turns = 0;
+        static final int END_OF_TURNS=1;
         //keep the game running until win/lose condition is met
         private boolean isGameOver = false;
         private String userInput;
@@ -23,11 +26,11 @@ public class PizzaQuestApp {
 
         public void execute() {
                 //temporaily put in a 1 iteration loop to test user input
-                while(turns <1) {
+                while(turns < END_OF_TURNS) {
                         welcome();
                         gameInstructions();
                         System.out.println(enterName());
-                        tp.parse(scanner.nextLine());
+                        parse(scanner.nextLine());
                         turns++;
                 }
         }
@@ -57,5 +60,44 @@ public class PizzaQuestApp {
                 isGameOver = true;
                 System.exit(0);
         }
+        private void parse(String userInput) {
+                //takes in user input and then splits it on the spaces. Logic comes later
+                List<String> parsedUserInput = new ArrayList<>(Arrays.asList(userInput.toLowerCase().split(" ")));
+                //after we break up the user iput send it to be process
+                processCommands(parsedUserInput);
+        }
+
+        //take the processed command and the delegates this to another
+        private void processCommands(List<String> verbAndNounList){
+
+                switch (verbAndNounList.get(0)) {
+                        case "quit":
+                                quitGame();
+                                break;
+                        case "go":
+                                go(verbAndNounList); // send to method to process next part of command
+
+                                break;
+                        case "look":
+                                look(); //player location or item  description printed
+
+                                break;
+                        case "take":
+                                addItem(); //take item from room if possible
+
+                                break;
+                        case "give":
+                                removeItem();
+                                break;
+                        case "inventory":
+                                printInventory(); // prints your inventory
+
+                                break;
+                        default:
+                                System.out.println("I don't understand");
+                                break;
+                }
+        }
+
 
 }
