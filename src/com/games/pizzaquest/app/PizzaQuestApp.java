9e0433649c2f99e0c650e260838deae9a89305dc
mlmirk <mike.l.mirkovic@gmail.com>
2022-06-13 09:16:50 -0700

    break out text parser from main app class, into new module. AB#67

--- src/com/games/pizzaquest/app/PizzaQuestApp.java
+++ src/com/games/pizzaquest/app/PizzaQuestApp.java
@@ -2,6 +2,7 @@ package com.games.pizzaquest.app;
 import com.games.pizzaquest.objects.Item;
 import com.games.pizzaquest.objects.Location;
 import com.games.pizzaquest.objects.Player;
+import com.games.pizzaquest.textparser.TextParser;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,13 +30,13 @@ public class PizzaQuestApp {
         public void setGameOver(boolean gameOver) {
                 isGameOver = gameOver;
         }
-        private final List<String> itemList = List.of("pizza_cutter", "prosciutto", "wine_glass", "lemons", "coin", "ancient_pizza_cookbook", "moped", "cannoli", "marble_sculpture", "espresso");
+        public final List<String> itemList = List.of("pizza_cutter", "prosciutto", "wine_glass", "lemons", "coin", "ancient_pizza_cookbook", "moped", "cannoli", "marble_sculpture", "espresso");
 
         //Initial State of the Player, inventory and starting location
-        private final Set<Item> inventory = new HashSet<>();
+        public final Set<Item> inventory = new HashSet<>();
         String[] rooms = {"rome", "paris" ,"nyc", "la"};
-        private final Location location =  new Location("Naples", rooms);
-        private final Player player = new Player(inventory, location);
+        public Location location =  new Location("Naples", rooms);
+        public Player player = new Player(inventory, location);
 
         //keep the game running until win/lose condition is met
         private boolean isGameOver = false;
@@ -43,7 +44,7 @@ public class PizzaQuestApp {
 
 
         public void execute() {
-
+                TextParser parser = new TextParser();
                 setGameOver(false);
                 //temporarily put in a 1 iteration loop to test user input
 
@@ -52,7 +53,8 @@ public class PizzaQuestApp {
                 System.out.println(enterName());
 
                 while(turns < END_OF_TURNS) {
-                        parse(scanner.nextLine());
+
+                        parser.parse(scanner.nextLine());
                         turns++;
                 }
         }
@@ -80,90 +82,13 @@ public class PizzaQuestApp {
                 return ("Ciao " + playerName+ " you are in " + player.getLocation());
         }
 
-        private void quitGame() {
+        public void quitGame() {
                 System.out.println("You'll always have a pizza our heart ... Goodbye!");
                 setGameOver(true);
                 System.exit(0);
         }
-        private void parse(String userInput) {
-                currentInput = userInput;
-                //takes in user input and then splits it on the spaces. Logic comes later
-                List<String> parsedUserInput = new ArrayList<>(Arrays.asList(userInput.toLowerCase().split(" ")));
-                //after we break up the user input send it to be process
-                processCommands(parsedUserInput);
-        }
 
-        //take the processed command and the delegates this to another
-        private void processCommands(List<String> verbAndNounList){
-                String noun = "";
-                if(verbAndNounList.size()>1){
-                       noun = verbAndNounList.get(verbAndNounList.size()-1);
-                }
-                String verb = verbAndNounList.get(0);
-
-
-
-                switch (verb) {
-                        case "quit":
-                                quitGame();
-                                break;
-                        case "go":
-                                if (noun.equals("")){
-                                        break;
-                                }
-                                //go(verbAndNounList); // send to method to process next part of command
-                                //we will need a valid location list to validate
-//                                String loc = verbAndNounList.get(1);
-                                player.setLocation(noun);
-                                break;
-                        case "look":
-                                //look(); //player location or item  description printed
-                                //will need a item list and a location list
-                                //todo - check size and get last
-                                //if room, do the first, else if item, do the second
-                                if (noun.equals("")){
-                                        break;
-                                }
-                                if(itemList.contains(noun)){
-                                        System.out.println(player.look(new Item(noun)));
-                                }
-                                else{
-                                        System.out.println(player.look(player.getLocation()));
-                                }
-                                break;
-                        case "take":
-                                //add item to inventory
-                                player.addToInventory(noun);
-                                break;
-                        case "give":
-                                //removes item from inventory
-                                if (noun.equals("")){
-                                        break;
-                                 }
-                                player.removeFromInventory(noun);
-                                break;
-                        case "inventory":
-
-                                Set<Item> tempInventory = player.getInventory();
-                                System.out.println("Items in the Inventory");
-                                for (Item item : tempInventory) {
-                                        System.out.println(item.getName());
-                                }
-                                break;
-                        case "help":
-                                gameInstructions();
-                                break;
-                        case "reset":
-                                resetGame();
-                                break;
-                        default:
-                                System.out.println(String.format("I don't understand '%s'", currentInput));
-                                gameInstructions();
-                                break;
-                }
-        }
-
-        private void resetGame() {
+        public void resetGame() {
                 setGameOver(true);
                 turns = 0;
                 execute();
--- src/com/games/pizzaquest/textparser/TextParser.java
+++ src/com/games/pizzaquest/textparser/TextParser.java
@@ -1,21 +1,99 @@
 package com.games.pizzaquest.textparser;
 
+import com.games.pizzaquest.app.PizzaQuestApp;
+import com.games.pizzaquest.objects.Item;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
-public class TextParser {
-
-    public String parse(String userInput) {
+public class TextParser extends PizzaQuestApp {
+    /*
+    A class that is designed to take in user input and call other function depending on
+    what the user types.
+         */
+    String currentInput="";
+    public void parse(String userInput) {
+        currentInput = userInput;
         //takes in user input and then splits it on the spaces. Logic comes later
-        ArrayList<String> temp = new ArrayList<>(Arrays.asList(userInput.toLowerCase().split(" ")));
-        //print user input
-        System.out.println(temp);
-
-        if (temp.get(0) == "quit" || temp.get(0) == "q" || temp.get(0) == "exit") {
-            System.out.println("You'll always have a pizza our heart ... Goodbye!");
-
-        }
-        return userInput;
+        List<String> parsedUserInput = new ArrayList<>(Arrays.asList(userInput.toLowerCase().split(" ")));
+        //after we break up the user input send it to be process
+        processCommands(parsedUserInput);
     }
+
+    //take the processed command and the delegates this to another
+    private void processCommands(List<String> verbAndNounList){
+        String noun = "";
+        if(verbAndNounList.size()>1){
+            noun = verbAndNounList.get(verbAndNounList.size()-1);
+        }
+        String verb = verbAndNounList.get(0);
+
+
+
+        switch (verb) {
+            case "quit":
+                quitGame();
+                break;
+            case "go":
+                if (noun.equals("")){
+                    break;
+                }
+                //go(verbAndNounList); // send to method to process next part of command
+                //we will need a valid location list to validate
+//                                String loc = verbAndNounList.get(1);
+                player.setLocation(noun);
+                break;
+            case "look":
+                //look(); //player location or item  description printed
+                //will need a item list and a location list
+                //todo - check size and get last
+                //if room, do the first, else if item, do the second
+                if (noun.equals("")){
+                    break;
+                }
+                if(itemList.contains(noun)){
+                    System.out.println(player.look(new Item(noun)));
+                }
+                else{
+                    System.out.println(player.look(player.getLocation()));
+                }
+                break;
+            case "take":
+                //add item to inventory
+                player.addToInventory(noun);
+                break;
+            case "give":
+                //removes item from inventory
+                if (noun.equals("")){
+                    break;
+                }
+                player.removeFromInventory(noun);
+                break;
+            case "inventory":
+
+                Set<Item> tempInventory = player.getInventory();
+                System.out.println("Items in the Inventory");
+                for (Item item : tempInventory) {
+                    System.out.println(item.getName());
+                }
+                break;
+            case "help":
+                gameInstructions();
+                break;
+            case "reset":
+                resetGame();
+                break;
+            default:
+                System.out.printf("I don't understand '%s'%n", currentInput);
+                gameInstructions();
+                break;
+        }
+    }
+
+
+
+
 
 }
